fe08b31581c02331ec963a46c05167d8279fce1a
Kaustubh Kale <kaustubhkale96@gmail.com>
2021-09-17 09:19:04 +0530

    Added Search

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -2,41 +2,91 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { apiKey, query } from '../keys';
 import './index.css';
+import { CloseOutlined, SearchOutlined } from '@ant-design/icons'
 
 export default function LandingPage() {
     const [image, setImage] = useState([]);
-    console.log(image)
+    const [search, setSearch] = useState([]);
+    const [Data, setData] = useState(true);
+    const [text, setText] = useState("");
+    console.log("images", image)
+    console.log("search=>", search)
+    console.log("search data=>", Data)
+    console.log()
     useEffect(() => {
         const getImage = () => {
-            return axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=&format=json&nojsoncallback=1&extras=url_s`)
+            return axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=26&format=json&nojsoncallback=1&extras=url_s`)
                 .then((response) => {
-                    console.log(response.data.photos.photo);
+                    console.log(response.data.photos);
                     setImage(response.data.photos.photo);
                 })
         }
         getImage();
     }, []);
 
+    const submit = (e) => {
+        e.preventDefault();
+        const searchText = image.filter((value) => {
+            return value.title.toLowerCase().includes(text.toLocaleLowerCase());
+
+        });
+        setSearch(searchText);
+        console.log("text", text)
+
+        if (searchText.length === 0) {
+            setData(false);
+        } else {
+            setData(true);
+        }
+    }
+
+    const searchImage = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        if (name === "search") {
+            setText(value);
+        }
+    }
+
     return (
         <React.Fragment>
             <div className="body">
                 <div className="container">
                     <div className="appbar">
-                        <h1 className="Title">PicsLab</h1>
+                        <a href="/">
+                            <h1 className="Title">PicsLab</h1>
+                        </a>
                     </div>
                     <form>
                         <div className="search">
-                            <input className="searchInput" type="text" placeholder="Search..." />
-                            <button>Search</button>
+                            <input className="searchInput" type="text" value={text} onChange={searchImage} placeholder="Search..." name="search" />
+                            <SearchOutlined className="button" onClick={submit} />
                         </div>
                     </form>
-                    <div className="gallery" >
-                        {image.map((item, index) => (
+                    <div>
+                        {Data === false && (
+                            <div >
+                                <h3 className="errorSearch">Sorry! no results for: {text}</h3>
+                            </div>
+                        )}
+                    </div>
+                    <div className="gallery">
+                        {search.length !== 0 ? search.map((item, index) => (
                             <div className="image" key={index}>
                                 <img src={item.url_s} alt="img" style={{ width: "100%", borderRadius: "5px" }} />
                             </div>
-                        ))}
+                        )) :
+                            <div>
+                                {image.map((item, index) => (
+                                    <div className="image" key={index}>
+                                        <img src={item.url_s} alt="img" style={{ width: "100%", borderRadius: "5px" }} />
+                                    </div>
+                                ))}
+                            </div>
+                        }
                     </div>
+
+
                 </div>
             </div>
         </React.Fragment>
--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
 const apiKey = '636e1481b4f3c446d26b8eb6ebfe7127';
-const query = 'tree, animal, mountain, car,'
+const query = 'trees, animals, mountains, cars'
 
 export { apiKey, query }
