8ea5483f44c83b3db65259bcb7e265257f7a96cb
Kaustubh Kale <kaustubhkale96@gmail.com>
2021-09-19 09:20:20 +0530

    first commit

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,55 +1,77 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { apiKey, query } from '../keys';
+import { apiKey } from '../keys';
 import './index.css';
-import { CloseOutlined, SearchOutlined } from '@ant-design/icons'
+import { CloseOutlined, SearchOutlined } from '@ant-design/icons';
 
 export default function LandingPage() {
     const [image, setImage] = useState([]);
-    const [search, setSearch] = useState([]);
-    const [Data, setData] = useState(true);
+    const [pages, setPages] = useState(1);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [tags, setTags] = useState("");
     const [text, setText] = useState("");
+
+    const TAGS = ["Abstract", "Flower", "Nature", "Car"]
+
     console.log("images", image)
-    console.log("search=>", search)
-    console.log("search data=>", Data)
-    console.log()
+    console.log("pages=>", pages)
+    console.log("pageNumber", pageNumber)
+    console.log("text", text)
+    console.log("tags", tags)
+
+
     useEffect(() => {
-        const getImage = () => {
-            return axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=26&format=json&nojsoncallback=1&extras=url_s,tags`)
-                .then((response) => {
-                    console.log(response.data.photos);
-                    setImage(response.data.photos.photo);
-                })
-        }
         getImage();
     }, []);
 
-    const submit = (e) => {
-        e.preventDefault();
-        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${text}&per_page=26&format=json&nojsoncallback=1&extras=url_s,tags`)
-            .then((res) => {
-                console.log(res)
-                const searchText = res.data.photos.photo.filter((item) => {
-                    console.log("value", item)
-                    return item.tags.toLowerCase().includes(text.toLowerCase());
-                });
-                setSearch(searchText);
-                console.log("text", text)
+    const getImage = (from = "default", tagsText) => {
+        let URL = "";
+        if (from === "default") {
+            URL = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&per_page=23&page=${pageNumber}&format=json&nojsoncallback=1&extras=url_s`
+        } else if (tagsText === "tags") {
+            URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&per_page=23&page=${pageNumber}&format=json&nojsoncallback=1&extras=url_s`
+        } else {
+            URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${text}&per_page=23&page=${pageNumber}&format=json&nojsoncallback=1&extras=url_s`
+        }
 
-                if (searchText.length === 0) {
-                    setData(false);
-                } else {
-                    setData(true);
-                }
+
+        axios.get(URL)
+            .then((res) => {
+                console.log('URL data=>', res)
+                setImage(res.data.photos.photo);
+                setPages(res.data.photos.pages);
+            })
+            .catch((err) => {
+                console.log(err)
             })
     }
+    const handleClick = (e) => {
+        e.preventDefault();
+        const { id } = e.target;
+        setTags({ tags: id === "clear" ? "" : id, pageNumber: 2 })
+        if (tags !== "") {
+            getImage("filter", "tags");
+            console.log("tags image", image)
+        } else {
+            getImage();
+            console.log("tags image", image)
+        }
 
+    }
     const searchImage = (e) => {
         e.preventDefault();
         const { name, value } = e.target;
         if (name === "search") {
             setText(value);
         }
+    }
+    const submit = (e) => {
+        e.preventDefault();
+        if (text !== "") {
+            setPageNumber(1);
+            getImage("search", "text");
+        }
+
     }
 
     return (
@@ -61,34 +83,41 @@ export default function LandingPage() {
                             <h1 className="Title">PicsLab</h1>
                         </a>
                     </div>
-                    <form>
+                    <form onSubmit={submit}>
                         <div className="search">
                             <input className="searchInput" type="text" value={text} onChange={searchImage} placeholder="Search..." name="search" />
-                            <SearchOutlined className="button" onClick={submit} />
+                            <span><SearchOutlined className="button" onClick={submit} /></span>
                         </div>
                     </form>
                     <div>
+                        {TAGS.map((item) => (
+                            <button id={item} onClick={handleClick}>{item}</button>
+                        ))}
+                        <button onClick={handleClick} id="clear">Clear</button>
+
+                    </div>
+                    {/* <div>
                         {Data === false && (
                             <div className="errorSearch">
                                 <h3>Sorry! no results for: {text}</h3>
                             </div>
                         )}
-                    </div>
+                    </div> */}
                     <div>
                         <div className="gallery">
-                            {search.length !== 0 ? search.map((item, index) => (
+                            {/* {search.length !== 0 ? search.map((item, index) => (
                                 <div className="image" key={index}>
                                     <img src={item.url_s} alt="img" style={{ width: "100%", borderRadius: "5px" }} />
                                 </div>
-                            )) :
-                                <div>
-                                    {image.map((item, index) => (
-                                        <div className="image" key={index}>
-                                            <img src={item.url_s} alt="img" style={{ width: "100%", borderRadius: "5px" }} />
-                                        </div>
-                                    ))}
-                                </div>
-                            }
+                            )) : */}
+                            <div>
+                                {image.map((item, index) => (
+                                    <div className="image" key={index}>
+                                        <img src={item.url_s} alt="img" style={{ width: "100%", borderRadius: "5px" }} />
+                                    </div>
+                                ))}
+                            </div>
+                            {/* } */}
                         </div>
                     </div>
                 </div>
